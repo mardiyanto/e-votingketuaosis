1ee49433032261514e86720d47dd88b750eaafe8
mardiyanto <mardybest@gmail.com>
2024-03-27 20:45:55 +0700

    fix

--- db_ketuaosis.sql
+++ db_ketuaosis.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2024 at 10:12 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 5.6.40
+-- Generation Time: 27 Mar 2024 pada 14.45
+-- Versi Server: 10.1.25-MariaDB
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,27 +25,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `paslon`
+-- Struktur dari tabel `paslon`
 --
 
 CREATE TABLE `paslon` (
   `id_paslon` int(100) NOT NULL,
   `nama_paslon` varchar(200) NOT NULL,
-  `foto` varchar(200) NOT NULL
+  `foto` varchar(200) NOT NULL,
+  `tgl_lahir` varchar(100) DEFAULT NULL,
+  `tmp_lahir` varchar(100) DEFAULT NULL,
+  `visi` text,
+  `misi` text,
+  `pendidikan` text,
+  `prestasi` text,
+  `token` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `paslon`
+-- Dumping data untuk tabel `paslon`
 --
 
-INSERT INTO `paslon` (`id_paslon`, `nama_paslon`, `foto`) VALUES
-(1, 'mardi', '1708590719_65d7067f03d06.jpg'),
-(2, 'g', '1708590710_product2.jpg');
+INSERT INTO `paslon` (`id_paslon`, `nama_paslon`, `foto`, `tgl_lahir`, `tmp_lahir`, `visi`, `misi`, `pendidikan`, `prestasi`, `token`) VALUES
+(5, 'AKABEST', '1709111256_almira.png', '2024-03-08', 'DGF', 'DG', 'DG', 'DG', 'DG', '44569017798372113322236485500047'),
+(6, 'ALMIRA NABILA FAUZI', '1709798218_ibu hamil warna ungu berhijab ada bunganya full body.png', '2024-03-07', '53535', 'SG', 'SG', '<p>FGSDG</p>\r\n', '<p>SGS</p>\r\n', '27470425673018322587995488871466');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pemilih`
+-- Struktur dari tabel `pemilih`
 --
 
 CREATE TABLE `pemilih` (
@@ -57,16 +64,17 @@ CREATE TABLE `pemilih` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `pemilih`
+-- Dumping data untuk tabel `pemilih`
 --
 
 INSERT INTO `pemilih` (`id_pemilih`, `nama_pemilih`, `nisn`, `no_hp`, `kelas`) VALUES
-(1, 'MARIYANTO', '878789', '0854754874', 'MOXI');
+(1, 'MARIYANTO', '878789', '0854754874', 'MOXI'),
+(2, 'aku', '15421', '90897878789', 'ok');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `profil`
+-- Struktur dari tabel `profil`
 --
 
 CREATE TABLE `profil` (
@@ -82,31 +90,40 @@ CREATE TABLE `profil` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `profil`
+-- Dumping data untuk tabel `profil`
 --
 
 INSERT INTO `profil` (`id_profil`, `nama_app`, `tahun`, `nama`, `alias`, `alamat`, `isi`, `gambar`, `akabest`) VALUES
-(1, 'PILKETOS', '2022/2023', 'DEWAN PENGAWAS PROGRAMING', 'DPRD PRINGSEWU', 'JL Wismarini No 09 Pringsewu Lampung', '', '22022024075913.jpg', 'mardybest@gmail.com'),
+(1, 'PILKETOS', '2022/2023', 'SMK NEGERI 2 PRINGSEWU', 'SMK NEGERI 2 PRINGSEWU', 'JL Wismarini No 09 Pringsewu Lampung', '', '22022024075913.jpg', 'mardybest@gmail.com'),
 (2, 're', '', 'MARDIYANTO', '19081989578978975', '', '', '', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `suara`
+-- Struktur dari tabel `suara`
 --
 
 CREATE TABLE `suara` (
   `id_suara` int(100) NOT NULL,
   `id_pemilih` int(100) NOT NULL,
   `id_paslon` int(100) NOT NULL,
-  `suara_sah` varchar(100) NOT NULL,
-  `suara_rusak` varchar(100) NOT NULL
+  `tahun` varchar(100) DEFAULT NULL,
+  `suara_sah` varchar(100) DEFAULT NULL,
+  `suara_rusak` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `suara`
+--
+
+INSERT INTO `suara` (`id_suara`, `id_pemilih`, `id_paslon`, `tahun`, `suara_sah`, `suara_rusak`) VALUES
+(1, 2, 5, '2024', '1', ''),
+(2, 1, 5, '2024', '1', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -118,7 +135,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`user_id`, `user_nama`, `user_username`, `user_password`, `user_foto`) VALUES
@@ -169,32 +186,27 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `paslon`
 --
 ALTER TABLE `paslon`
-  MODIFY `id_paslon` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `id_paslon` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `pemilih`
 --
 ALTER TABLE `pemilih`
-  MODIFY `id_pemilih` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+  MODIFY `id_pemilih` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `profil`
 --
 ALTER TABLE `profil`
   MODIFY `id_profil` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
 --
 -- AUTO_INCREMENT for table `suara`
 --
 ALTER TABLE `suara`
-  MODIFY `id_suara` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
-
+  MODIFY `id_suara` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-COMMIT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
